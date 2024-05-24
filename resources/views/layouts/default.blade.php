bc54d4f42a3c3ae4db0731c7e3b6ee6dc31f9ad6
pingk0504 <t103310075@ntut.org.tw>
2024-05-24 10:08:53 +0000

    模板切分

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 新手入門課程</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Weibo App</title>
-</head>
-<body>
-  <h1>帮助页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('title', '幫助頁')
+@section('content')
+  <h1>幫助頁</h1>
+@stop
