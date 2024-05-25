8f0acc1311313bf55e49e457180460367b6529b5
pingk0504 <t103310075@ntut.org.tw>
2024-05-25 15:29:46 +0000

    初始化樣式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.default')
-@section('title', '主頁')
 
 @section('content')
-  <h1>主頁</h1>
+  <div class="bg-light p-3 p-sm-5 rounded">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      你現在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入門課程</a> 的範例項目首頁。
+    </p>
+    <p>
+      一切，將從這裡開始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">現在註冊</a>
+    </p>
+  </div>
 @stop
