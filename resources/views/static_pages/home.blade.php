f72d02de6888f0b1fe9cf7b400895bb45793c7c2
pingk0504 <t103310075@ntut.org.tw>
2024-06-05 07:39:29 +0000

    用戶註冊頁面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,7 +10,7 @@
       一切，將從這裡開始。
     </p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">現在註冊</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">現在註冊</a>
     </p>
   </div>
 @stop
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.default')
-@section('title', '幫助頁')
+@section('title','註冊')
+
 @section('content')
-  <h1>幫助頁</h1>
+<h1>註冊<h1>
+
 @stop
